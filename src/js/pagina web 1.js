ff794f7cebeda79b0513427a31f0a5da14441e99
sserrato21 <soiaserratomejia@gmail.com>
2020-11-12 20:32:39 -0500

    header y footer (base) finalizados
    Esta seria la estructura para el header y footer de las otras paginas, lo unico que varia es el color

--- src/js/pagina web 1.js
+++ src/js/pagina web 1.js
@@ -1,1 +1,16 @@
-alert('hola');
+// alert('hola');
+
+// Interaccion 1
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.dropdown-trigger');
+    let instances = M.Dropdown.init(elems, options);
+  });
+
+// ----------------------
+
+// Interaccion 2
+
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
