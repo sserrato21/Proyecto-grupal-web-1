13c28e24d19623712ccb741336ee6dd62785c331
sserrato21 <soiaserratomejia@gmail.com>
2020-11-13 22:00:30 -0500

    Corregi ciertas cosas de la seccion main, los tamaños de la grilla que se utilizo para materialize en algunas secciones estaba incorrecta

--- src/js/pagina web 1.js
+++ src/js/pagina web 1.js
@@ -1,5 +1,9 @@
 // alert('hola');
 
+
+
+// Interaccion 1
+
 document.getElementById("btnabrir").addEventListener("click",function(){
   document.getElementsByClassName("fondo_transparente")[0].style.display="flex" 
   return false
@@ -9,31 +13,33 @@ document.getElementsByClassName("modal_cerrar")[0].addEventListener("click",func
    document.getElementsByClassName("fondo_transparente")[0].style.display="none" 
 });
 
-// Interaccion 1
+// Interaccion 2
 
 document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.dropdown-trigger');
-    let instances = M.Dropdown.init(elems, options);
-  });
+  let elems = document.querySelectorAll('.dropdown-trigger');
+  let instances = M.Dropdown.init(elems, options);
+});
 
 // ----------------------
 
-// Interaccion 2
+// Interaccion 3
 
 $(document).ready(function() {
     $('input#input_text, textarea#textarea2').characterCounter();
   });
 
 
-  // Interaccion 4
-  $(document).ready(function() {
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true
-  });
+// // Interaccion 4 
+// $(document).ready(function() {
+//   $('.carousel.carousel-slider').carousel({
+//     fullWidth: true
+//   });
+
+// -------hubo un problema con la interaccion 4, al estar activa, la interaccion 1 deja de funcionar de repente--------
+// -------por eso la puse como un comentario (en texto verde) mientras tanto porque al parecer hay algo interfiriendo con la interaccion 1 (la del modal)--------
 
 
-
-  // Interaccion 5
+//   // Interaccion 5
   $(document).ready(function(){
     $('.carousel').carousel();
   });
